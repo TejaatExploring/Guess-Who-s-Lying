e4aa32e2433b18730d8f25322af687d8246e91fc
TejaatExploring <bhanuteja279@gmail.com>
2025-07-25 13:59:09 +0530

    adjusted db model's

--- server/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema({
-  socketId: { type: String, required: true },
-  userName: { type: String, required: true },
-  roomId: { type: String, required: true }
-});
-
-module.exports = mongoose.model('User', UserSchema);
